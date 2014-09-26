0753a8929ae33f0b7708967d120c5df2c88e45f8
Andrew <andyjacuw@gmail.com>
2014-09-26 14:48:48 -0700

    brute force solution

--- firstNonRepeating.js
+++ firstNonRepeating.js
@@ -3,7 +3,7 @@ function firstNonRepeating(text) {
 
   for (i = 0; i < len; i++) {
     currentChar = text.charAt(i);
-    if (text.lastIndexOf(currentChar, i) === i) {
+    if (text.indexOf(currentChar) === i && text.indexOf(currentChar, i + 1) === -1) {
       return currentChar;
     }
   }
