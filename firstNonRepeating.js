d0af11adf407e13fbebbeedc0da2169dfb2833ac
Andrew <andyjacuw@gmail.com>
2014-09-27 13:32:26 -0700

    working function passes all tests

--- firstNonRepeating.js
+++ firstNonRepeating.js
@@ -1,19 +1,26 @@
 function firstNonRepeating(text) {
   var len = text.length, space = ' ', charTracker = {}, i, currentChar, letterInObject;
-  if (len === 0) {
+  if (!text) {
     return '';
   }
   for (i = 0; i < len; i++) {
     currentChar = text[i];
-    letterInObject = charTracker["letter " + currentChar];
+    letterInObject = charTracker[currentChar];
     if (letterInObject && currentChar !== space) {
-      charTracker["letter " + currentChar] += 1;
+      charTracker[currentChar] += 1;
     } else if (!letterInObject && currentChar !== space) {
-      charTracker["letter " + currentChar] = 1;
+      charTracker[currentChar] = 1;
     }
   }
-  console.log(charTracker);
+  for (i in charTracker) {
+    if (charTracker[i] === 1) {
+      return i;
+    }
+  }
+  return '';
 }
 
-console.log(firstNonRepeating("a a aab c debfg"));
+console.log(firstNonRepeating('a a aab c debfg'));
 console.log(firstNonRepeating(''));
+console.log(firstNonRepeating('abc  abc a      bc g abcabf g '));
+console.log(firstNonRepeating('  '));
